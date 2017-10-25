14ae39acf5104f16e82076c6bf1a0052ddd97fd2
chazmcgrill <charlietaylordev@gmail.com>
2017-10-25 12:21:17 +0100

    Add Marked Library
    Added the marked library which converts markdown to html and assigned to variable in the app file. Also removed unnecessary line in the webpack config.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Marked from 'marked';
+
+console.log();
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Markdown Previewer</h1>
+        { Marked('mark down __test__.') }
       </div>
     )
   }
