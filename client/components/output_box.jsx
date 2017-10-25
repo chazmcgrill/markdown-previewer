9e3aaf40bb987a237709210d3454d72ded646dbf
chazmcgrill <charlietaylordev@gmail.com>
2017-10-25 15:18:12 +0100

    Add Basic Styling
    Added basic styling and linked on the html file cleaning up the class names also.

--- client/components/output_box.jsx
+++ client/components/output_box.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class OutputBox extends Component {
   render () {
     return (
-      <div className="output">Sample Text</div>
+      <div className="output-data">Sample Text</div>
     )
   }
 }
--- client/index.js
+++ client/index.js
@@ -8,12 +8,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Markdown Previewer</h1>
         <div className="container">
-          <div>
+          <div className="box input-box">
             <InputBox />
           </div>
-          <div>
+          <div className="box output-box">
             <OutputBox />
           </div>
         </div>
